00887a14f3cdca63e5d346660f6bf6517af02b6e
Himanshu choudhary <you@example.com>
2024-01-03 14:34:42 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,10 @@ export default function Home() {
 
   const [gift, setGift] = useState("");
   const [text, setText] = useState("");
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState(
+    "Select the behaviours you wish to commit to this year:"
+  );
 
   const [click, setClick] = useState("");
   const [loading, setloading] = useState(true);
@@ -67,10 +71,12 @@ export default function Home() {
       const countYes = allOption.filter((option) => option != "").length;
       const countNo = allOption.filter((option) => option === "No").length;
 
-      setText(
-        "Yay, thanks for commiting to Swiggy's Blended Ways of Working! Now, unlike your New Year's Resolutions, follow the selected behaviours & become a Blended Champ! "
+      setText("Yay, Thanks for commiting to Swiggy's Blended Ways of Working!   🙏 ");
+      setText1(
+        " Now, unlike your New Year's Resolutions, follow the selected behaviours & become a Blended Champ! 🤩"
       );
-      setGift("Click to Know More about Blended Ways of Working.");
+      setText2("");
+      setGift("Click to Know More about Blended Ways of Working");
       setClick("/image/gif.gif");
 
       setloading(true);
@@ -84,34 +90,38 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen py-4 bg-transparent">
-        <h1 className="text-[2rem] pb-2 font-semibold text-white">
-          {" "}
-          Select the behaviours you wish to commit to this year
-        </h1>
         <form
           action="/"
           id="myForm"
           onSubmit={handleSubmit}
-          className={` flex-col  bg-white p-8  shadow-md md:w-[90%] transition-all duration-500 rounded-[12px] m-5`}
+          className={
+            (text2 === "" ? "mt-5" : "") +
+            ` flex-col  bg-white p-8  shadow-md md:w-[90%] transition-all duration-500 rounded-[12px] m-5`
+          }
         >
+          <div className="mx-auto">
+            <h1 className="text-[20px] pb-2 font-bold my-3"> {text2}</h1>
+          </div>
+
           <div
             className={`${
               gift != "" ? "hidden" : "flex "
-            } flex-col transition-all duration-500`}
+            } flex-col transition-all duration-500  text-gray-700`}
           >
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"A"}
                 name="option1"
-                className="mr-2"
+                className="mr-2 font-bold"
                 onChange={(e) => {
                   setoprtion1(e.target.value);
                 }}
               />
-              I am a cheer champ - sprinkling words of appreciation when due.
+              <p> <span className="font-semibold">I am a cheer champ </span> - sprinkling words of appreciation when due. </p>
+              
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"B"}
@@ -119,12 +129,13 @@ export default function Home() {
                 onChange={(e) => {
                   setoption2(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am a self-care superhero - i plan my day / leaves, have lunch on
-              time, block deep work hours etc.
+              <p><span className="font-semibold">I am a self-care superhero </span>- i plan my day / leaves, have lunch on
+              time, block deep work hours etc.</p>
+               
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"C"}
@@ -132,12 +143,14 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion3(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am a work wizard - sticking to committed deadlines, and
-              proactively communicating a likely miss.
+              <p><span  className="font-semibold">I am a work wizard</span>  - sticking to committed deadlines, and
+              proactively communicating a likely miss.              
+              </p>
+               
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"D"}
@@ -145,12 +158,13 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion4(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I care for others - making intentional efforts to know my
-              colleagues beyond work.
+              <p><span className="font-semibold"> I care for others </span>- making intentional efforts to know my
+              colleagues beyond work.</p>
+             
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"E"}
@@ -158,12 +172,13 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion5(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am a camera chief - I switch on my camera for making hybrid
-              meetings humane
+              <p><span className="font-semibold"> I am a camera chief </span>- I switch on my camera for making hybrid
+              meetings humane</p>
+             
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"C"}
@@ -171,11 +186,12 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion6(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am a team player - I proactively ask for help and offer help.
+              <p><span className="font-semibold">I am a team player</span> - I proactively ask for help and offer help.</p>
+              
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"D"}
@@ -183,13 +199,14 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion7(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am a communication chef - blending synchronous (meetings,calls)
+              <p><span className="font-semibold">I am a communication chef </span> - blending synchronous (meetings,calls)
               and async (slack, mail, chat) channels for effective
-              collaboration.
+              collaboration.</p>
+             
             </label>
-            <label className="block mb-4">
+            <label className=" mb-4 flex items-center">
               <input
                 type="checkbox"
                 value={"E"}
@@ -197,45 +214,57 @@ export default function Home() {
                 onChange={(e) => {
                   setoprtion8(e.target.value);
                 }}
-                className="mr-2"
+                className="mr-2 font-bold"
               />
-              I am tech savvy - using the right tools & resources to get work
-              done and upskill myself.
+              <p><span className="font-semibold">I am tech savvy </span>- using the right tools & resources to get work
+              done and upskill myself.</p>
+              
             </label>
-            <label className="block mb-4">
-              <span className="block mb-1">Email:</span>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email Address"
-                className="w-full p-2 border border-gray-300 rounded"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                required
-              />
+            <label className=" my-4  flex-col items-center gap-3">
+              <span className="block mb-1 text-[18px] font-semibold">Email:</span>
+              <div className="flex items-center gap-2 w-[50%]">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your Swiggy mail id"
+                  className="p-2 border border-gray-300 rounded w-full py-2 "
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  required
+                />
+                <input
+                  type="submit"
+                  value={loading ? "Submit " : "Loading..."}
+                  onSubmit={handleSubmit}
+                  className="bg-[#ED8a22]  text-white p-2  cursor-pointer rounded font-bold w-[200px] "
+                />
+              </div>
             </label>
-            <input
-              type="submit"
-              value={loading ? "Submit " : "Loading..."}
-              onSubmit={handleSubmit}
-              className="bg-green-500 text-white p-2 w-full rounded cursor-pointer"
-            />
           </div>
           {/* <h1 className="pt-3">{click}</h1> */}
           <img
             className={`flex justify-center mx-auto cursor-pointer w- ${
-              click != "" ? "flex [600px] h-[450px]" : "hidden"
+              click != "" ? "flex [300px] h-[300px]" : "hidden"
             } `}
             src={click}
           />
-          <div className="flex  flex-col items-center justify-center text-center">
-            <p>{text}</p>
+          <div className="flex  flex-col items-center justify-center text-center mt-5">
+            <p className="text-[18px] font-bold">{text}</p>
+            <p>{text1}</p>
             <Link
-              href="https://swiggy.advantageclub.co/"
+              href="https://sites.google.com/swiggyverse.com/intranet/our-blended-ways-of-working"
               className="pt-4 flex justify-center capitalize "
             >
-              <span className="text-orange-500 font-bold"> {gift}</span>
+              <span
+                className={
+                  (gift === "" ? "hidden" : "block") + 
+                      ` font-bold bg-[#ED8a22] px-4 py-2 rounded-full text-white`
+                }
+              >
+                {" "}
+                {gift}
+              </span>
             </Link>
           </div>
         </form>
